66701324c68fdc1f75d16f76fcd03adfea88599a
kermani <97584991+hkermanin@users.noreply.github.com>
2026-02-27 16:43:47 +0000

    project_7

---
+++ src/bin/7.rs
@@ -0,0 +1,12 @@
+use std::{thread, time::Duration};
+
+fn main(){
+
+    let handle = thread::spawn(|| {
+        println!("Spawn");
+        thread::sleep(Duration::from_millis(3000));
+    });
+
+    println!("salam");
+    handle.join().unwrap();
+}
